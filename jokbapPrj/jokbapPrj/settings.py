0ff339e5ea48cf03123b48dd887975c818dd5b6d
joseunghyo <seunghyo7742@naver.com>
2021-09-03 21:59:43 +0900

    템플릿 적용

--- jokbapPrj/jokbapPrj/settings.py
+++ jokbapPrj/jokbapPrj/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'jokbapPrj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['jokbapPrj/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
